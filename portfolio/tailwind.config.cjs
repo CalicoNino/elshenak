796c4045b85ef471186c5913dde67a81372ac494
elshenak <elshenak@gmail.com>
2023-03-23 04:30:15 -0400

    About & styles

--- portfolio/tailwind.config.cjs
+++ portfolio/tailwind.config.cjs
@@ -5,9 +5,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
+        primary: "#c72c41",
+        secondary: "#801336",
+        tertiary: "#510a32",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
