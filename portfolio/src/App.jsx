04aba26cea2dcab0705f2cf597a97e76001f1229
elshenak <elshenak@gmail.com>
2024-01-06 18:23:51 -0500

    updates

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
           <Intro />
         </div>
         <About />
-        <Experience />
+        {/* <Experience /> */}
         <Tech />
         {/* <Works /> */}
         <Certifications />
